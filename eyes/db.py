997704d84c299231ce2fd56012d38be77af82bc7
Sean <seanchang@Seans-Mac-Mini.local>
2021-08-04 14:44:27 +0800

    Add Comment ORM base model

--- eyes/db.py
+++ eyes/db.py
@@ -18,10 +18,29 @@ class PttPost(Base, Timestamp):
     )
 
 
+class PttComment(Base, Timestamp):
+    '''Ptt comment ORM model
+    '''
+    __tablename__ = 'ptt_comments'
+
+    id = sa.Column(sa.String(64), primary_key=True)
+
+
 class DcardPost(Base, Timestamp):
     '''Dcard post ORM model
     '''
     __tablename__ = 'dcard_posts'
+
+    id = sa.Column(
+        sa.String(64),
+        primary_key=True,
+    )
+
+
+class DcardComment(Base, Timestamp):
+    '''Dcard comment ORM model
+    '''
+    __tablename__ = 'dcard_comments'
 
     id = sa.Column(
         sa.String(64),
